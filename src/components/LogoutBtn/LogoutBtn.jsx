823426d7c9fa62f6a38f2c186913093debabba2f
speedy-hussu <speedyhussu.work@gmail.com>
2025-06-03 11:24:20 +0530

    login css issue

--- src/components/LogoutBtn/LogoutBtn.jsx
+++ src/components/LogoutBtn/LogoutBtn.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import authService from "../../appwrite/auth";
 import { logout } from "../../redux/authSlice";
-import "./logoutBtn.css";
+import "./LogoutBtn.css";
 
 function LogoutBtn() {
   const dispatch = useDispatch();
