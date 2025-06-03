383376092ac580313b199dc68b1b22eefba8a043
speedy-hussu <speedyhussu.work@gmail.com>
2025-06-03 11:59:15 +0530

    config

--- src/config/config.js
+++ src/config/config.js
@@ -1,19 +1,9 @@
 const config = {
-  appWriteUrl: String(
-    (import.meta.env.VITE_APPWRITE_URL = "https://fra.cloud.appwrite.io/v1")
-  ),
-  appWriteProjectId: String(
-    (import.meta.env.VITE_APPWRITE_PROJECT_ID = "682cbd47002276e46a04")
-  ),
-  appWriteDatabaseId: String(
-    (import.meta.env.VITE_APPWRITE_DATABASE_ID = "682cbde7003de4774f49")
-  ),
+  appWriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+  appWriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+  appWriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
 
-  appWriteCollectionId: String(
-    (import.meta.env.VITE_APPWRITE_COLLECTION_ID = "682cbe0a00300200c58f")
-  ),
-  appWriteBucketId: String(
-    (import.meta.env.VITE_APPWRITE_BUCKET_ID = "682cbf81000882969578")
-  ),
+  appWriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+  appWriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
 };
 export default config;
